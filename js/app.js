997b555808fdb2f8b1f0017157305e50b3a1f752
muhammadrakib2299 <muhammad.rakib2299@gmail.com>
2025-02-23 01:17:54 +0600

    works on js file donate amount proecess

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log("hello from js file");
+// Donate amount process by using donate button
+document.getElementById('btn-donate').addEventListener('click', function(e){
+    e.preventDefault();
+    // get the amount user given the input fill
+    const getDonateAmount = document.getElementById('input-donate-amount').value;
+    // get the current donation amount status
+    const donateAmountStatus = document.getElementById('donate-balance-status').innerText;
+
+    if(getDonateAmount >= 0 && getDonateAmount > -getDonateAmount ){
+        // additoin the given amount
+        const updateAmountStatus = parseFloat(donateAmountStatus) + parseFloat(getDonateAmount);
+        document.getElementById('donate-balance-status').innerText = updateAmountStatus; 
+        document.getElementById('input-donate-amount').value = ' ';
+    }
+    else{
+        alert("Donation do not sucessful, Try again")
+    }
+})
